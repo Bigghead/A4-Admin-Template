f60dc899fa0a32388cb9e7e317b4e729c24702de
Bigghead <shavenchew@yahoo.com>
2018-02-28 14:10:15 -0800

    breaking up into modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,27 @@
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
 
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { DashHelper } from './dashboard/dashboard-helper';
 import { GalleryComponent } from './gallery/gallery.component';
+import { DashboardModule } from './dashboard/index';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent,
     GalleryComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    SharedModule,
+    DashboardModule
   ],
-  providers: [ DashHelper ],
+  providers: [ ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
